419839d594cfbf3ec7c0e37c5165eec74dcbe86e
Vasyl Sokolyk <vsokolyk@gmail.com>
2018-09-06 02:04:05 +0300

    Initial commit

---
+++ Config.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace JazzMan\APP_Config;
+
+use JazzMan\ParameterBag\ParameterBag;
+use JazzMan\Traits\SingletonTrait;
+
+/**
+ * Class Config
+ *
+ * @package JazzMan\APP_Config
+ */
+class Config
+{
+
+    use SingletonTrait;
+
+    /**
+     * @var \JazzMan\ParameterBag\ParameterBag
+     */
+    private $config;
+
+    public function __construct()
+    {
+
+        $config = apply_filters('wp_app_config', []);
+
+        $this->config = new ParameterBag($config);
+    }
+
+    /**
+     * @return ParameterBag
+     */
+    public function getConfig()
+    {
+        return $this->config;
+    }
+}
---
+++ helper.php
@@ -0,0 +1,12 @@
+<?php
+
+if ( ! function_exists('app_config')) {
+
+    /**
+     * @return \JazzMan\ParameterBag\ParameterBag
+     */
+    function app_config()
+    {
+        return JazzMan\APP_Config\Config::getInstance()->getConfig();
+    }
+}
