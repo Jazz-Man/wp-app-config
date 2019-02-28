0d94a53e1f2b0ec2504c32bec82c8920779263b7
Jazz <vsokolyk@gmail.com>
2019-02-28 23:01:49 +0000

    added functions for working with paths

--- helper.php
+++ helper.php
@@ -9,3 +9,32 @@ if (!function_exists('app_config')) {
         return JazzMan\APP_Config\Config::getInstance()->getConfig();
     }
 }
+
+if (!function_exists('app_dir_patch')) {
+    /**
+     * @param string $patch
+     *
+     * @return string
+     */
+    function app_dir_patch($patch = '')
+    {
+        $patch = trim($patch, DIRECTORY_SEPARATOR);
+
+        return app_config()->get('root_dir').DIRECTORY_SEPARATOR.$patch;
+    }
+}
+
+if (!function_exists('app_url_patch')) {
+    /**
+     * @param string $patch
+     *
+     * @return string
+     */
+    function app_url_patch($patch = '')
+    {
+        $patch = trim($patch, DIRECTORY_SEPARATOR);
+
+        return app_config()->get('root_url').DIRECTORY_SEPARATOR.$patch;
+    }
+}
+
