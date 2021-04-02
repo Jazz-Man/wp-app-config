3b9d7ee55bf5c8e7e40fe00bb5ff1eb303d8473f
Vasyl Sokolyk <vsokolyk@gmail.com>
2021-04-02 16:28:46 +0300

    add app_use_webp function

--- helper.php
+++ helper.php
@@ -27,3 +27,36 @@ if (!function_exists('app_url_patch')) {
         return app_config()->get('root_url').'/'.$path;
     }
 }
+
+if (!\function_exists('app_use_webp')) {
+    function app_use_webp(): bool
+    {
+        if (!empty($_SERVER['HTTP_ACCEPT']) && false !== \strpos($_SERVER['HTTP_ACCEPT'], 'image/webp')) {
+            return true;
+        }
+
+        $UA = !empty($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : false;
+
+        if ($UA) {
+            if (false !== \strpos($UA, ' Chrome/')) {
+                return true;
+            }
+
+            if (false !== \strpos($UA, 'Safari') && ($result = \stristr($UA, 'Version'))) {
+                \preg_match('/(?:version\/(?<version>[\d.]+))?/i', $result, $matches);
+                if (!empty($matches['version']) && \version_compare($matches['version'], '13', '>=')) {
+                    return true;
+                }
+            }
+
+            if (false !== \strpos($UA, 'Firefox')) {
+                $result = \explode('/', \stristr($UA, 'Firefox'));
+                if (!empty($result[1]) && \version_compare($result[1], '65', '>=')) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
