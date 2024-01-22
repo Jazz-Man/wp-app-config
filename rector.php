8f5e29ca087dfca8908e1cf91cf379cc830c7256
Vasyl Sokolyk <vsokolyk@gmail.com>
2024-01-22 16:44:33 +0200

    style(rector): update Rector config

--- rector.php
+++ rector.php
@@ -1,13 +1,8 @@
 <?php
 
-use Rector\CodeQuality\Rector\Array_\CallableThisArrayToAnonymousFunctionRector;
-use Rector\CodeQuality\Rector\Empty_\SimplifyEmptyCheckOnEmptyArrayRector;
-use Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector;
 use Rector\Config\RectorConfig;
-use Rector\Php71\Rector\FuncCall\RemoveExtraParametersRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
-use Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector;
 
 return static function ( RectorConfig $config ): void {
     $config->sets( [
@@ -17,6 +12,9 @@ return static function ( RectorConfig $config ): void {
         SetList::EARLY_RETURN,
         SetList::NAMING,
         SetList::PRIVATIZATION,
+        SetList::INSTANCEOF,
+        SetList::DEAD_CODE,
+        SetList::STRICT_BOOLEANS,
         LevelSetList::UP_TO_PHP_82,
     ] );
 
@@ -25,23 +23,13 @@ return static function ( RectorConfig $config ): void {
     $config->removeUnusedImports();
     $config->importShortClasses( false );
     $config->parallel();
-    //    $config->cacheDirectory(__DIR__.'/cache/rector');
     $config->phpstanConfig( __DIR__.'/phpstan-rector.neon' );
 
     $config->paths( [
         __DIR__.'/src',
-        __DIR__.'/wp-performance.php',
     ] );
 
     $config->skip( [
         __DIR__.'/vendor',
-        __DIR__.'/cache',
-        CallableThisArrayToAnonymousFunctionRector::class,
-        //        ClassConstantToSelfClassRector::class,
-        RemoveExtraParametersRector::class,
-        EncapsedStringsToSprintfRector::class,
-        DisallowedEmptyRuleFixerRector::class,
-        SimplifyEmptyCheckOnEmptyArrayRector::class,
-        //	    ArrayShapeFromConstantArrayReturnRector::class,
     ] );
 };
